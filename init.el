d9c369bdd4a62a0d7c04f7a113e658fc5e7034c6
cleanbaja <circutrider21@outlook.com>
2021-11-14 14:05:03 -0600

    Added rust support and refractored language support

--- init.el
+++ init.el
@@ -4,14 +4,37 @@
   "piratemacs.org"
   user-emacs-directory))
 
-;; Load the golang config
+;; Load the generic language config
 (org-babel-load-file
  (expand-file-name
-  "lang-go.org"
+  "lang/generic.org"
+  user-emacs-directory))
+
+;; Uncomment to enable golang support
+;(org-babel-load-file
+; (expand-file-name
+;  "lang/rust.org"
+;  user-emacs-directory))
+
+;; Comment to disable rust support
+(org-babel-load-file
+ (expand-file-name
+  "lang/rust.org"
   user-emacs-directory))
 
 ;; Some lsp stuff...
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(lsp-mode which-key use-package smart-mode-line simple-modeline nano-modeline go-mode general evil-collection doom-themes doom-modeline dashboard airline-themes)))
 
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
